e0d6aa50ed3ab364fd7d1dc63adccdae5fec6e74
unknown <Wardan Ahmed>
2024-05-19 00:02:56 +0500

    push my q no 5

---
+++ app.js
@@ -0,0 +1,3 @@
+var famous_person = "Quaid-e-Azam";
+var message = "we are not a sindhi we are not a pungabi and we are not a pathan we are all muslims";
+console.log("".concat(famous_person, " once said, ").concat(message));
---
+++ app.ts
@@ -0,0 +1,3 @@
+let famous_person = "Quaid-e-Azam";
+let message = "we are not a sindhi we are not a punjabi and we are not a pathan we are all muslims";
+console.log(`${famous_person} once said, ${message}`);
